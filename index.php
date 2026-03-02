4bddcad65c5a93ee5f92a1ed2df7ecc3cca54ba3
kamisoris <romainbouchard1401@gmail.com>
2026-03-02 17:36:48 +0100

    mise en place

--- index.php
+++ index.php
@@ -3,10 +3,17 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/style.css">
     <title>Document</title>
 </head>
 <body>
-    
+    <header>
+        <nav>
+            <ul>
+                <li><a href="login.php"><img src="img/login.png" alt="login"></a></li>
+                </ul>
+        </nav>
+    </header>
 </body>
 </html>
 <?php
