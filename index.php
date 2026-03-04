11c3eb3663f9796cde0bdf3b73befc32ca637f6e
romain <romain@gmail.com>
2026-03-04 10:59:02 +0100

    avancement tentative placement boite connexion

--- index.php
+++ index.php
@@ -11,19 +11,23 @@
     <header>
         <nav>
             <ul>
-                <li><a href="#" id="login"><img src="img/login.png" alt="login"></a></li>
+                <li>
+                    <a href="#" id="login"><img src="img/login.png" alt="login"></a>
+                    <div id="loginbox">
+                        <form>
+                            <label> nom utilisateur
+                                <input type="text" name="nom_utilisateur" id="user_name">
+                            </label>
+                            <label> mot de passe
+                                <input type="password" id="password">
+                            </label>
+                            <input type="submit" value="connexion">
+                        </form>
+                        <ul><li id="inscription"><a href="inscription.php">s'inscrire</a></li></ul>
+                    </div>
+                </li>
                 </ul>
         </nav>
-        <div id="loginbox">
-            <form>
-                <label> nom utilisateur</label>
-                <input type="text" name="nom_utilisateur" id="user_name">
-                <label> mot de passe </label>
-                <input type="password" id="password">
-                <input type="submit" value="connexion">
-            </form>
-            <li id="inscription"><a href="inscription.php">s'inscrire</a></li>
-        </div>
     </header>
 </body>
 </html>
--- script/script.js
+++ script/script.js
@@ -4,9 +4,9 @@ const loginbox = document.getElementById("loginbox");
 loginli.addEventListener("click", function (event)
 {
     event.preventDefault(); //empêche rechargement de la page
-    loginbox.style.display = "block"; //afficher la boîte
-    const positionlien = loginli.getBoundingClientRect(); //calcul ou est le lien a écran
-    //placement de la boîte
-    loginbox.style.top = (positionlien.bottom + window.scrollY) + 'px';
-    loginbox.style.left = (positionlien.left + window.scrollX) + 'px';
+    if (loginbox.style.display === "block") {
+        loginbox.style.display = "none";
+    } else {
+        loginbox.style.display = "block";
+    }
 });
