6679628567977a18c2c49475f3c86747694fb795
romain <romain@gmail.com>
2026-03-04 12:48:21 +0100

    progression formulaire inscription

--- inscription.php
+++ inscription.php
@@ -8,12 +8,22 @@
 <body>
     <form>
         <label>inscription</label>
-        <label><br>nom utilisateur</label>
-        <input type="text" name="nom_utilisateur" id="user_name">
-        <label> mot de passe </label>
-        <input type="password" id="password">
-        <label> confirmer mot de passe </label>
-        <input type="password" id="confirm_password">
+        <label>
+            <br>nom utilisateur
+            <input type="text" name="nom_utilisateur" id="user_name">
+        </label>
+        <label>
+            <br>email
+            <input type="email" name="email" id="user_mail">
+        </label>
+        <label>
+            mot de passe
+            <input type="password" id="password" name="password">
+        </label>
+        <label>
+            confirmer mot de passe
+            <input type="password" id="confirm_password" name="confirm_password">
+        </label>
         <input type="submit" value="s'inscrire">
     </form>
 </body>
@@ -21,4 +31,38 @@
 <?php
 require 'bdd.php';
 connexionbdd();
+if ($_POST['nom_utilisateur'] != "" && $_POST['email'] != "" && $_POST['password'] != "" && $_POST['confirm_password'] !== $_POST['password'] )
+{
+    if (isset($_POST['nom_utilisateur'],$_POST['email'],$_POST['password'],$_POST['confirm_password']))
+    {
+        if (empty($_POST['nom_utilisateur']))
+        {
+            echo "nom utilisateur est vide";
+        }
+        elseif (empty($_POST['email']))
+        {
+            echo "email est vide";
+        }
+        elseif (empty($_POST['password']))
+        {
+            echo "mot de passe est vide";
+        }
+        elseif (empty($_POST['confirm_password']))
+        {
+            if ($_POST['password'] != $_POST['confirm_password'])
+            {
+                echo "les mot de passe ne correspondent pas";
+            }
+            else {
+                echo "confirm mot de passe est vide";
+            }
+        }
+    }
+}
+else
+{
+    echo "une erreur c'est produite:";
+
+}
+
 ?>
