65b1a7d8a8e64da5044e8c9054a4ce6d3a5fcafa
romain <romain@gmail.com>
2026-03-05 13:02:08 +0100

    fin formulaire inscription

--- inscription.php
+++ inscription.php
@@ -8,68 +8,69 @@
 <body>
 <?php
 require 'bdd.php';
-if (isset($_POST['nom_utilisateur'],$_POST['email'],$_POST['password']))
-{
-    if (empty($_POST['nom_utilisateur']))
-    {
-        echo "nom utilisateur est vide";
-    }
-    elseif (!preg_match('/^[\p{L}0-9 ._-]+$/u'))
-    {
-        echo "Le nom d'utilisateur ne peut contenir que des lettres, des chiffres, des espaces et des tirets ou point  (- ou _ ou .)";
-    }
-    elseif (empty($_POST['email']))
-    {
-        echo "email est vide";
-    }
-    elseif (empty($_POST['password']))
-    {
-        echo "mot de passe est vide";
-    }
-    elseif (empty($_POST['confirm_password']))
-    {
-        echo "confirm mot de passe est vide";
-    }
-    elseif ($_POST['password'] != $_POST['confirm_password'])
-    {
-        echo "les mots de passe ne corespondent pas";
-    }
-    elseif (strlen($_POST['nom_utilisateur'])>20)
-    {
-        echo "le nom utilisateur doit être au maximum 20 caractère";
-    }
-    else
-    {
-        $blindgamedb = connexionbdd();
-        $verification_nom_utilisateur = $blindgamedb->prepare("SELECT id_joueur FROM compte  WHERE username = ?");
-        $verification_nom_utilisateur->execute([$_POST['nom_utilisateur']]);
-        if ($verification_nom_utilisateur ->rowCount() > 0)
-        {
-            echo "ce nom utilisateur est déja utiliser";
+
+if (isset($_POST['submit'])) {
+    if (isset($_POST['nom_utilisateur'], $_POST['email'], $_POST['password'], $_POST['confirm_password'])) {
+
+        if (empty($_POST['nom_utilisateur'])) {
+            echo "nom utilisateur est vide";
         }
-        else{
-            $crypted_password = password_hash($_POST['password'], PASSWORD_DEFAULT);
-            $request = $blindgamedb->prepare("INSERT INTO compte (username,email,password) VALUES (?,?,?)");
-            $insertionn_reussie = $request->execute([
+        elseif (strlen($_POST['nom_utilisateur']) > 20) {
+            echo "le nom utilisateur doit être au maximum 20 caractères";
+        }
+        elseif (!preg_match('/^[\p{L}0-9 ._-]+$/u', $_POST['nom_utilisateur'])) {
+            echo "Le nom d'utilisateur contient des caractères non autorisés.";
+        }
+        elseif (empty($_POST['email'])) {
+            echo "email est vide";
+        }
+        elseif (!filter_var($_POST['email'], FILTER_VALIDATE_EMAIL)) {
+            echo "email n'est pas valide";
+        }
+        elseif (empty($_POST['password'])) {
+            echo "mot de passe est vide";
+        }
+        elseif (empty($_POST['confirm_password'])) {
+            echo "confirm mot de passe est vide";
+        }
+        elseif ($_POST['password'] != $_POST['confirm_password']) {
+            echo "les mots de passe ne correspondent pas";
+        }
+        else {
+            $blindgamedb = connexionbdd();
+            $verification_nom_utilisateur = $blindgamedb->prepare("SELECT id_joueur FROM compte WHERE username = ?");
+            $verification_nom_utilisateur->execute([$_POST['nom_utilisateur']]);
+
+            $verification_email = $blindgamedb->prepare("SELECT email FROM compte WHERE email = ?");
+            $verification_email->execute([$_POST['email']]);
+
+            if ($verification_nom_utilisateur->rowCount() > 0) {
+                echo "ce nom utilisateur est déjà utilisé";
+            }
+            elseif ($verification_email->rowCount() > 0) {
+                echo "cet email est déjà rattaché à un compte";
+            }
+            else {
+                $crypted_password = password_hash($_POST['password'], PASSWORD_DEFAULT);
+                $request = $blindgamedb->prepare("INSERT INTO compte (username, email, password) VALUES (?, ?, ?)");
+
+                $insertionn_reussie = $request->execute([
                     $_POST['nom_utilisateur'],
                     $_POST['email'],
                     $crypted_password
-            ]);
-            if ($insertionn_reussie)
-            {
-                echo "Votre compte à bien était crée";
-            }
-            else{
-                echo "Une erreur est survenue lors de la création du compte";
+                ]);
+
+                if ($insertionn_reussie) {
+                    echo "Votre compte a bien été créé !";
+                } else {
+                    echo "Une erreur est survenue lors de la création du compte";
+                }
             }
         }
     }
+} else {
+    echo "Veuillez remplir le formulaire.";
 }
-else
-{
-    echo "formulaire non envoyer";
-}
-
 ?>
     <form method="POST">
         <label>inscription</label>
