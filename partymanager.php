915ec7f2a92054ad48d31198883b1851dc2655c6
romain <romain@gmail.com>
2026-03-09 15:44:18 +0100

    construction de la classe partymanager

---
+++ partymanager.php
@@ -0,0 +1,26 @@
+<?php
+$blindgamedb = connexionbdd();
+class partymanager
+{
+    private $score;
+    private $bdd;
+    private $vie;
+    public function __construct($conexionBdd,$viedepart)
+    {
+        $this->bdd = $conexionBdd;
+        $this->score = 0;
+        $this->vie = $viedepart;
+    }
+    public function compter_score($dificulte)
+    {
+
+    }
+    public function sauvgarder_score()
+    {
+
+    }
+    public function perdre_vie($vie)
+    {
+
+    }
+}
